5edc3d93b71d433e65baebd49c4689ef7057ca76
Rafi Laksmana <trlaksma@iu.edu>
2019-09-09 11:08:08 -0400

    create dummy

---
+++ app/public/dummy.php
@@ -0,0 +1,31 @@
+{
+  "patientGuid": "01";
+  "firstName": "Rafi";
+  "lastName": "Laksmana";
+  "dob": "04/23/1997"
+  "sexAtBirth": "Male"
+}
+
+{
+  "patientGuid": "02";
+  "firstName": "Isaac";
+  "lastName": "Bolin";
+  "dob": "09/23/1996"
+  "sexAtBirth": "Male"
+}
+
+{
+  "patientGuid": "03";
+  "firstName": "Bjorn";
+  "lastName": "Liow";
+  "dob": "02/20/1996"
+  "sexAtBirth": "Male"
+}
+
+{
+  "patientGuid": "04";
+  "firstName": "Quincy";
+  "lastName": "Ballard";
+  "dob": "03/17/1997"
+  "sexAtBirth": "Female"
+}
