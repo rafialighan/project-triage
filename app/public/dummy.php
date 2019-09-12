93aa2f868eb1fca2fbe96c1550b55a504f0c29c2
Rafi Laksmana <trlaksma@iu.edu>
2019-09-11 20:35:18 -0400

    js, css, html
    copy everything from tom's repo

---
+++ app/public/dummy.php
@@ -0,0 +1,26 @@
+[
+  {
+    "patientGuid": "SOME-REALLY-LONG-1234",
+    "firstName": "Rafi",
+    "lastName": "Laksmana",
+    "dob": "1997-04-23",
+    "sexAtBirth": "M",
+    "priority": "high"
+  },
+  {
+    "patientGuid": "SOME-REALLY-SHORT-5678",
+    "firstName": "Quincy",
+    "lastName": "Ballard",
+    "dob": "1997-03-17",
+    "sexAtBirth": "F",
+    "priority": "medium"
+  },
+  {
+    "patientGuid": "SOME-UNIQUE-ABCDE1",
+    "firstName": "Isaac",
+    "lastName": "Bolin",
+    "dob": "1997-09-23",
+    "sexAtBirth": "M",
+    "priority": "low"
+  }
+]
